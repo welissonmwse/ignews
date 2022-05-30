dce54d0147cbe4d05c4972c681062ea58ae30788
Welisson Silveira <welissonmwse@gmail.com>
2022-05-30 19:49:06 -0300

    Adicionando estilo global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+    </>
+  )
 }
